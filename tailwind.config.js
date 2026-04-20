281484907dcf260449df3568853dd6dac36cc684
Soheil Ahmadi <soheilahmadijalal.sj1378@gmail.com>
2026-04-20 14:47:01 +0330

    complete mobile design for entire site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html",
-    "./static/src/**/*.js"
-  ],
+  content: ["./templates/**/*.html", "./static/src/**/*.js"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(circle, var(--tw-gradient-stops))',
-      }
+        "gradient-radial": "radial-gradient(circle, var(--tw-gradient-stops))",
+      },
+      colors: {
+        gray: {
+          25: "#f8f8f8",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
