7cd735c3981f3b808bfccea579671d54483c9e6e
SoheilJA <soheilahmadijalal.sj1378@gmail.com>
2026-07-10 22:38:53 +0330

    create services views.py

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from .models import Service, ServicesPageSection
 
-# Create your views here.
+
+def services_list(request):
+    context = {
+        "page_section": ServicesPageSection.objects.first(),
+        "services": Service.objects.all(),
+    }
+    return render(request, "services/list.html", context)
+
+
+def services_detail(request, slug):
+    service = get_list_or_404(Service, slug=slug)
+    context = {
+        "service": service,
+        "stats": service.stats.all(),
+        "why_us": service.why_us.all(),
+        "deliverables": service.deliverables.all(),
+        "processes": service.processes.all(),
+        "plans": service.plans.all(),
+        "custom_items": service.costom_item.all(),
+        "faqs": service.faqs.all(),
+        "testimonials": service.testimonials.all(),
+        "other_services": service.objects.exclude(slug=slug)[:4],
+    }
+    return render(request, "services/detail.html", context)
