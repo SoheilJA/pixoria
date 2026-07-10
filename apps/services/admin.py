7e48c273a2b86a45743d64c4d4be5bad01633926
Soheil JalalAhmadi <soheilahmadijalal.sj1378@gmail.com>
2026-07-10 05:57:15 +0330

    create services admin.py

--- apps/services/admin.py
+++ apps/services/admin.py
@@ -1,3 +1,91 @@
 from django.contrib import admin
+from .models import (
+    ServicesPageSection,
+    Service,
+    ServiceStat,
+    ServiceWhyUs,
+    ServiceDeliverable,
+    ServiceProcess,
+    ServicePlan,
+    ServicePlanFeature,
+    ServiceCustomItem,
+    ServiceFAQ,
+    ServiceTestimonial,
+)
 
-# Register your models here.
+
+@admin.register(ServicesPageSection)
+class ServicesPageSectionAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+
+    def has_add_permission(self, request):
+        return not ServicesPageSection.objects.exists()
+
+
+class ServiceStatInline(admin.TabularInline):
+    model = ServiceStat
+    extra = 1
+
+
+class ServiceWhyUsInline(admin.TabularInline):
+    model = ServiceWhyUs
+    extra = 1
+
+
+class ServiceDeliverableInline(admin.TabularInline):
+    model = ServiceDeliverable
+    extra = 1
+
+
+class ServiceProcessInline(admin.TabularInline):
+    model = ServiceProcess
+    extra = 1
+
+
+class ServicePlanFeatureInline(admin.TabularInline):
+    model = ServicePlanFeature
+    extra = 1
+
+
+class ServicePlanInline(admin.StackedInline):
+    model = ServicePlan
+    extra = 1
+    show_change_link = True
+
+
+class ServiceCustomItemInline(admin.TabularInline):
+    model = ServiceCustomItem
+    extra = 1
+
+
+class ServiceFAQInline(admin.TabularInline):
+    model = ServiceFAQ
+    extra = 1
+
+
+class ServiceTestimonialInline(admin.TabularInline):
+    model = ServiceTestimonial
+    extra = 1
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["title", "order", "featured"]
+    list_editable = ["order", "featured"]
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [
+        ServiceStatInline,
+        ServiceWhyUsInline,
+        ServiceDeliverableInline,
+        ServiceProcessInline,
+        ServicePlanInline,
+        ServiceCustomItemInline,
+        ServiceFAQInline,
+        ServiceTestimonialInline,
+    ]
+
+
+@admin.register(ServicePlan)
+class ServicePlanAdmin(admin.ModelAdmin):
+    list_display = ["service", "name", "plan_type", "price", "is_featured"]
+    inlines = [ServicePlanFeatureInline]
