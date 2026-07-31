b53facec908c3fa67a7d8fef11ccf604afc41daf
Soheil JalalAhmadi <soheilahmadijalal.sj1378@gmail.com>
2026-07-31 20:04:32 +0330

    fix copyright in mobile

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&i^(qejd(3ad!89zlcdeezds717=dmpfas4vmp5f9(lh3=a1*v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", 'localhost','django-avazeh-e2m-avazeh.runflare.cloud']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "django-avazeh-k3p-avazeh.runflare.cloud"]
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
 
